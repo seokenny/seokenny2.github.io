0bacab668bd371674ca38846d7cf0e22fce3a484
Kenny Seo <44145254+seokenny@users.noreply.github.com>
2019-03-18 00:03:43 -0400

    Almost complete

--- js/app.js
+++ js/app.js
@@ -42,6 +42,32 @@ $(document).ready(function(){
 			$("#about_contain").addClass("active");
 		};
 	});
+	//Mobile Nav
+	$(".m_nav_home").on("click",function(){
+		if($("#work_contain").hasClass("active")){
+			$("#work_contain").stop().fadeOut("fast",function(){
+				$("#contact_contain").fadeOut("fast",function(){
+					$("#about_contain").fadeIn("fast");
+				});			
+			});
+			$("#work_contain").removeClass("active");
+			$(".wt1").css("display","none");
+			$(".wt2").css("display","none");
+			$(".wt3").css("display","none");
+			$(".wt4").css("display","none");
+			$("#about_contain").addClass("active");
+		}
+		else if($("#contact_contain").hasClass("active")){
+			$("#contact_contain").stop().fadeOut("fast",function(){
+				$("#contact_contain").fadeOut("fast",function(){
+					$("#about_contain").fadeIn("fast");
+				});			
+			});
+			$("#contact_contain").removeClass("active");
+			$("#about_contain").addClass("active");
+		};
+		hideMobile();
+	});
 
 	$(".nav_works").on("click",function(){
 		if($("#about_contain").hasClass("active")){
@@ -76,6 +102,42 @@ $(document).ready(function(){
 		};
 	});
 
+	//Mobile Works
+
+	$(".m_nav_works").on("click",function(){
+		if($("#about_contain").hasClass("active")){
+			$("#about_contain").stop().fadeOut("fast",function(){
+				$("#work_contain").fadeIn("fast",function(){
+					$(".wt1").fadeIn(125,function(){
+						$(".wt2").fadeIn(125,function(){
+							$(".wt3").fadeIn(125,function(){
+								$(".wt4").fadeIn(125);
+							});
+						});
+					});
+				});
+			});
+			$("#about_contain").removeClass("active");
+			$("#work_contain").addClass("active");
+		}
+		else if($("#contact_contain").hasClass("active")){
+			$("#contact_contain").stop().fadeOut("fast",function(){
+				$("#work_contain").fadeIn("fast",function(){
+					$(".wt1").fadeIn(125,function(){
+						$(".wt2").fadeIn(125,function(){
+							$(".wt3").fadeIn(125,function(){
+								$(".wt4").fadeIn(125);
+							});
+						});
+					});
+				});
+			});
+			$("#about_contain").removeClass("active");
+			$("#work_contain").addClass("active");
+		};
+		hideMobile();
+	});
+
 	$(".nav_contact").on("click",function(){
 		if($("#work_contain").hasClass("active")){
 			$("#about_contain").stop().fadeOut("fast",function(){
@@ -101,6 +163,34 @@ $(document).ready(function(){
 		};
 	});
 
+	//Mobile Contact
+
+	$(".m_nav_contact").on("click",function(){
+		if($("#work_contain").hasClass("active")){
+			$("#about_contain").stop().fadeOut("fast",function(){
+				$("#work_contain").fadeOut("fast",function(){
+					$("#contact_contain").fadeIn("fast");
+				});			
+			});
+			$("#work_contain").removeClass("active");
+			$(".wt1").css("display","none");
+			$(".wt2").css("display","none");
+			$(".wt3").css("display","none");
+			$(".wt4").css("display","none");
+			$("#contact_contain").addClass("active");
+		}
+		else if($("#about_contain").hasClass("active")){
+			$("#about_contain").stop().fadeOut("fast",function(){
+				$("#work_contain").fadeOut("fast",function(){
+					$("#contact_contain").fadeIn("fast");
+				});			
+			});
+			$("#about_contain").removeClass("active");
+			$("#contact_contain").addClass("active");
+		};
+		hideMobile();
+	});
+
 	//RUGS USA
 	$(".wt1").on("click",function(){
 		$(".work_tile").stop().fadeOut(function(){
@@ -110,7 +200,7 @@ $(document).ready(function(){
 		});
 	});
 
-	$(".work_select_contain").on("click",function(){
+	$(".work_header").on("click",function(){
 		$(".rugsusa_contain").stop().fadeOut();
 		$("#work_full").stop().animate({left:'100%'},750,function(){
 			showTiles();
@@ -127,7 +217,7 @@ $(document).ready(function(){
 		});
 	});
 
-	$(".work_select_contain").on("click",function(){
+	$(".work_header").on("click",function(){
 		$(".guesser_contain").stop().fadeOut();
 		$("#work_full").stop().animate({left:'100%'},750,function(){
 			showTiles();
@@ -144,7 +234,7 @@ $(document).ready(function(){
 		});
 	});
 
-	$(".work_select_contain").on("click",function(){
+	$(".work_header").on("click",function(){
 		$(".mystiq_contain").stop().fadeOut();
 		$("#work_full").stop().animate({left:'100%'},750,function(){
 			showTiles();
@@ -163,6 +253,13 @@ $(document).ready(function(){
 		wt3_fade();
 		wt4_fade();
 	});
+
+	$(".nav_works").on("click",function(){
+		wt1_fade();
+		wt2_fade();
+		wt3_fade();
+		wt4_fade();
+	});	
 
 	$(".nav_contact").on("click",function(){
 		wt1_fade();
@@ -201,5 +298,27 @@ $(document).ready(function(){
 	function wt4_fade(){
 		$(".guesser_contain").stop().fadeOut();
 		$("#work_full").stop().animate({left:'100%'},750);
+	}
+
+	/**MOBILE MENU**/
+	$(".bmo").on("click",function(){
+		$(".mobile_nav").stop().slideDown(500,function(){
+			$(".mb_nav_bar").fadeIn(250);
+			$(".bmx").fadeIn(250);
+		});
+	});
+
+	$(".bmx").on("click",function(){
+		$(".bmx").stop().fadeOut(250);
+		$(".mb_nav_bar").stop().fadeOut(250,function(){
+			$(".mobile_nav").stop().slideUp(250);
+		});
+	});
+
+	function hideMobile(){
+		$(".bmx").stop().fadeOut(250);
+		$(".mb_nav_bar").stop().fadeOut(250,function(){
+			$(".mobile_nav").stop().slideUp(250);
+		});
 	}
 });
